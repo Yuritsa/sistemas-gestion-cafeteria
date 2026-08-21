cbdc1d47224ede649c0178d390686b6af126e191
YuriPaez <ingrithyuritsapaez@gmail.com>
2026-08-20 20:31:03 -0600

    update de seguridad para los datos y el http

--- cafeteriayuri/settings.py
+++ cafeteriayuri/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #Middleware personalizado para auditoría de seguridad
+    'pedidos.middleware.AuditoriaSeguridadMiddleware',
 ]
 
 ROOT_URLCONF = 'cafeteriayuri.urls'
@@ -122,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+#configuración de archivos multimedia (Media Files)
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
